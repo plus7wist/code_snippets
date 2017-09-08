5638d39de826d23c839031d87cbc35075eb8fd41
plus7wist <gu_castle@163.com>
2017-09-08 12:48:18 +0800

    update cmake build system: -Wall and CMAKE_BUILD_TYPE

--- opencv/build.sh
+++ opencv/build.sh
@@ -1,6 +1,18 @@
 #! /usr/bin/env bash
 
 build=cmake_build
+cmake_build_type=Debug
+
+if test $# -ge 1; then
+    cmake_build_type=$1
+fi
+
+case $cmake_build_type in
+    None|Debug|Release|RelWithDebInfo|MinSizeRel);;
+    *)
+        echo Invalid CMAKE_BUILD_TYPE: $cmake_build_type;
+        exit -1;;
+esac
 
 if test -d $build; then
     rm -rf $build
@@ -8,5 +20,5 @@ fi
 
 mkdir $build
 cd $build
-cmake .. >/dev/null 2>&1
+cmake -DCMAKE_BUILD_TYPE=$cmake_build_type .. >/dev/null 2>&1
 make
